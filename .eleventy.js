fe099c4cd307e762827126ef3fc8b5490beb4887
Blair Currey <curreyb88@gmail.com>
2021-12-15 00:07:10 -0500

    update info, hide old content, formatting

--- .eleventy.js
+++ .eleventy.js
@@ -1,33 +1,38 @@
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
-const markdownItAttrs = require('markdown-it-attrs');
+const markdownItAttrs = require("markdown-it-attrs");
 // const CleanCSS = require("clean-css");
 
-module.exports = (function(eleventyConfig) {
-
+module.exports = function (eleventyConfig) {
     //Set additional files to copy over to output.
     eleventyConfig.addPassthroughCopy("src/js");
-    eleventyConfig.addPassthroughCopy({ "src/scss/purged/main.css": "css/main.css" });
+    eleventyConfig.addPassthroughCopy({
+        "src/scss/purged/main.css": "css/main.css",
+    });
     eleventyConfig.addPassthroughCopy("src/img");
 
     //Add filters
     // eleventyConfig.addFilter("cssmin", function(code) {
     //     return new CleanCSS({}).minify(code).styles;
     //   });
-    eleventyConfig.addFilter("readableDate", dateObj => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('MM/dd/yyyy');
+    eleventyConfig.addFilter("readableDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+            "MM/dd/yyyy"
+        );
     });
-    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-        return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LLL-dd');
+    eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+            "yyyy-LLL-dd"
+        );
     });
     eleventyConfig.addFilter("sortByOrder", (values) => {
-        let vals = [...values];     // this *seems* to prevent collection mutation...
+        let vals = [...values]; // this *seems* to prevent collection mutation...
         return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
     });
-    eleventyConfig.addFilter('markdown', function(value) {
-        let markdown = require('markdown-it')({
-            html: true
+    eleventyConfig.addFilter("markdown", function (value) {
+        let markdown = require("markdown-it")({
+            html: true,
         });
         return markdown.render(String(value));
     });
@@ -36,35 +41,36 @@ module.exports = (function(eleventyConfig) {
     eleventyConfig.setFrontMatterParsingOptions({
         excerpt: true,
         // Optional, default is "---"
-        excerpt_separator: "<!-- excerpt -->"
+        excerpt_separator: "<!-- excerpt -->",
     });
 
     //Template Engines
     let markdownLibrary = markdownIt({
         html: true,
         breaks: true,
-        linkify: true
-    }).use(markdownItAnchor, {
-        permalink: false,
-        // permalinkClass: "direct-link",
-        permalinkSymbol: "#"
-    }).use(markdownItAttrs, {
-        leftDelimiter: '{<', // default '{'
-        rightDelimiter: '>}', // default '}'
-        allowedAttributes: ['id', 'class'] // empty = all allowed
-    });
+        linkify: true,
+    })
+        .use(markdownItAnchor, {
+            permalink: false,
+            // permalinkClass: "direct-link",
+            permalinkSymbol: "#",
+        })
+        .use(markdownItAttrs, {
+            leftDelimiter: "{<", // default '{'
+            rightDelimiter: ">}", // default '}'
+            allowedAttributes: ["id", "class"], // empty = all allowed
+        });
     eleventyConfig.setLibrary("md", markdownLibrary);
 
     return {
-
         dir: {
             input: "src",
             output: "_site",
             include: "_includes",
             layout: "_layouts",
-            data: "_data"
+            data: "_data",
         },
-    
-        templateFormats: ["html", "njk", "md"]
+
+        templateFormats: ["html", "njk", "md"],
     };
-});
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
 //Remember the theme selection
-const themeSwitch = document.querySelector('#theme-switch');
-themeSwitch.checked = localStorage.getItem('switchedTheme') === 'true';
+const themeSwitch = document.querySelector("#theme-switch");
+themeSwitch.checked = localStorage.getItem("switchedTheme") === "true";
 
-themeSwitch.addEventListener('change', function (e) {
-  if(e.currentTarget.checked === true) {
-    // Add item to localstorage
-    localStorage.setItem('switchedTheme', 'true');
-  } else {
-    // Remove item if theme is switched back to normal
-    localStorage.removeItem('switchedTheme');
-  }
+themeSwitch.addEventListener("change", function (e) {
+    if (e.currentTarget.checked === true) {
+        // Add item to localstorage
+        localStorage.setItem("switchedTheme", "true");
+    } else {
+        // Remove item if theme is switched back to normal
+        localStorage.removeItem("switchedTheme");
+    }
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip({
-        placement : 'top'
+        placement: "top",
     });
 });
